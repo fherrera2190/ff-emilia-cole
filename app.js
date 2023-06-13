71fef8d5a87da28e729dc528bd3932b63a0b898d
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-06-13 12:42:12 -0300

    feat: iniciar estructura

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3030;
+const path = require('path');
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+app.get('/contacto', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+app.get('/music', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'music.html'));
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el http://localhost:${PORT}`);
+});
