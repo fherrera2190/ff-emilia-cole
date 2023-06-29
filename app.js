0ee09cf390873ef33e6365c888851c18ae44a33a
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-06-29 02:41:16 -0300

    Se agrego CSS a cada seccion

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const app = express();
 const PORT = 3030;
 const path = require('path');
 
+//Configuraciones
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 app.get('/contacto', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+    res.sendFile(path.join(__dirname, 'views', 'contacto.html'));
 });
 app.get('/music', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'music.html'));
@@ -18,7 +24,6 @@ app.get('/about', (req, res) => {
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el http://localhost:${PORT}`);
